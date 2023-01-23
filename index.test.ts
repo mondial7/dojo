a68532e6e66e1c5bf0215cf8dae7304471f160ec
Marco Mondini <m.mondini@ubutler.nl>
2023-01-23 21:42:03 +0100

    ✨ template ts/jest

---
+++ index.test.ts
@@ -0,0 +1,11 @@
+import { doNothing } from ".";
+
+describe("Kata Template", () => {
+  it("does nothing...", () => {
+    expect(doNothing("")).toBeTruthy();
+  });
+
+  it("replies to politeness", () => {
+    expect(doNothing("please")).toBeFalsy();
+  });
+});
---
+++ index.ts
@@ -0,0 +1,3 @@
+export const doNothing = (beingPolite) => {
+  return !beingPolite;
+};
