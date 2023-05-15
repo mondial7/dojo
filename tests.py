5e2d412cf2df00ca8749ca3bdce718e4a6283e7c
Marco Mondini <m.mondini@ubutler.nl>
2023-05-15 15:32:13 +0200

    last

--- tests.py
+++ tests.py
@@ -8,22 +8,3 @@
 # The class Point has no Getters
 # The class Point has no public properties (i.e. the internal state cannot be read from outside the class).
 # 
-
-class Point:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-
-
-def manhattanDistance(pointA: Point, pointB: Point) -> int:
-    return 0
-
-
-def test_starting_point_to_itself():
-    startingPoint: Point = Point(1, 1)
-    assert manhattanDistance(startingPoint, startingPoint) == 0
-
-def test_starting_point_to_point_b():
-    startingPoint: Point = Point(1, 1)
-    pointB: Point=Point(1,2)
-    assert manhattanDistance(startingPoint, startingPoint) == 0
