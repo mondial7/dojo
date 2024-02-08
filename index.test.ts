403fe1f9c3de2ac22c5380c881d29a3fff44db71
Marco Mondini <m.mondini@ubutler.nl>
2024-02-08 17:14:12 +0100

    refactoring tests

--- index.test.ts
+++ index.test.ts
@@ -21,36 +21,53 @@
 import { GameScore, Card, CardValues, Suits } from "."
 
 describe('GameScore', () => {
+  describe("calculate", () => {
+    describe("when the cards are all different suits", () => {
+      it.each([
+        [CardValues.Seven, CardValues.Seven, CardValues.Seven, 7],
+        [CardValues.Seven, CardValues.Eight, CardValues.Eight, 8],
+        [CardValues.Seven, CardValues.Eight, CardValues.Nine, 9],
+        [CardValues.Seven, CardValues.Eight, CardValues.Ten, 10],
+        [CardValues.Seven, CardValues.Nine, CardValues.Jack, 10],
+        [CardValues.Seven, CardValues.Nine, CardValues.Queen, 10],
+        [CardValues.Seven, CardValues.Nine, CardValues.King, 10],
+        [CardValues.Seven, CardValues.Eight, CardValues.Ace, 11],
+      ])("should return the value of the highest", (cardAValue, cardBValue, cardCValue, expectedScore) => {
+        const cardA = new Card(Suits.Spades, cardAValue)
+        const cardB = new Card(Suits.Clovers, cardBValue)
+        const cardC = new Card(Suits.Hearts, cardCValue)
 
-  describe("getScore", () => {
-    it.each([
-      [CardValues.Seven, CardValues.Seven, CardValues.Seven, 7],
-      [CardValues.Seven, CardValues.Eight, CardValues.Eight, 8],
-      [CardValues.Seven, CardValues.Eight, CardValues.Nine, 9],
-      [CardValues.Seven, CardValues.Eight, CardValues.Ten, 10],
-      [CardValues.Seven, CardValues.Nine, CardValues.Jack, 10],
-      [CardValues.Seven, CardValues.Nine, CardValues.Queen, 10],
-      [CardValues.Seven, CardValues.Nine, CardValues.King, 10],
-      [CardValues.Seven, CardValues.Eight, CardValues.Ace, 11],
-    ])("should return the value of the highest card when given different suits", (cardAValue, cardBValue, cardCValue, expectedScore) => {
-      const cardA = new Card(Suits.Spades, cardAValue)
-      const cardB = new Card(Suits.Clovers, cardBValue)
-      const cardC = new Card(Suits.Hearts, cardCValue)
+        const score = GameScore.calculate([cardA, cardB, cardC])
 
-      const score = GameScore.calculate([cardA, cardB, cardC])
-
-      expect(score).toBe(expectedScore)
+        expect(score).toBe(expectedScore)
+      })
     })
 
-    it('2 same suits should add up', () => {
-      const cardA = new Card(Suits.Spades, CardValues.Eight)
-      const cardB = new Card(Suits.Spades, CardValues.Eight)
-      const cardC = new Card(Suits.Hearts, CardValues.Jack)
-
-      const score = GameScore.calculate([cardA, cardB, cardC])
-
-      expect(score).toBe(16)
+    describe("when the cards are all of the same suits", () => {
+      it('cards should add up', () => {
+        const cardA = new Card(Suits.Spades, CardValues.Eight)
+        const cardB = new Card(Suits.Spades, CardValues.Seven)
+        const cardC = new Card(Suits.Spades, CardValues.Nine)
+  
+        const score = GameScore.calculate([cardA, cardB, cardC])
+  
+        expect(score).toBe(24)
+      })
     })
+    
+    describe("When the cards have mixed suits", ()=>{
+      it("should sum only the cards with same suits", () => {
+        const cardA = new Card(Suits.Spades, CardValues.Eight)
+        const cardB = new Card(Suits.Clovers, CardValues.Seven)
+        const cardC = new Card(Suits.Spades, CardValues.Nine)
+  
+        const score = GameScore.calculate([cardA, cardB, cardC])
+  
+        expect(score).toBe(17)
+      })
+    })
+
+        
   })
   
 
